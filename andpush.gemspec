5f81565b88ecbc7df6e6c496a495689d35af3917
Yuki Nishijima <yk.nishijima@gmail.com>
2017-05-29 18:22:35 -0400

    Use connection pool to improve performance

--- andpush.gemspec
+++ andpush.gemspec
@@ -15,6 +15,8 @@ Gem::Specification.new do |spec|
   spec.files         = `git ls-files -z`.split("\x0").reject {|f| f.match(%r{^(test)/}) }
   spec.require_paths = ["lib"]
 
+  spec.add_dependency 'net-http-persistent'
+
   spec.add_development_dependency "bundler"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "minitest"
--- lib/andpush.rb
+++ lib/andpush.rb
@@ -1,12 +1,14 @@
+require 'net/http/persistent'
+
 require 'andpush/version'
 require 'andpush/client'
 
 module Andpush
   DOMAIN = 'https://fcm.googleapis.com'.freeze
 
-  def self.build(server_key, domain: nil)
+  def self.build(server_key, domain: nil, request_handler: ConnectionPool.new)
     ::Andpush::Client
-      .new(domain || DOMAIN)
+      .new(domain || DOMAIN, request_handler: request_handler)
       .register_interceptor(Authenticator.new(server_key))
   end
 
@@ -21,4 +23,21 @@ module Andpush
       [uri, body, headers, options]
     end
   end
+
+  class ConnectionPool
+    attr_reader :connection
+
+    def initialize(name: nil, proxy: nil, pool_size: Net::HTTP::Persistent::DEFAULT_POOL_SIZE)
+      @connection = Net::HTTP::Persistent.new(name: name, proxy: proxy, pool_size: pool_size)
+    end
+
+    def call(request_class, uri, headers, body, *_)
+      req = request_class.new(uri, headers)
+      req.set_body_internal(body)
+
+      connection.request(uri, req)
+    end
+  end
+
+  private_constant :Authenticator, :ConnectionPool
 end
